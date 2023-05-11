2d7fad38b1171cbb30f0e14ae116e804998707ba
MarioSantoro <mariosantoro124@gmail.com>
2023-05-11 20:58:54 +0200

    add function on button

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,16 +1,19 @@
 // L'ho fatto qui e non in html per sperimentare
-document.getElementById("title").innerHTML = "Generatore password migliore al mondo (Nessuno può contraddire questa affermazione!!!!)";
+document.getElementById("title").innerHTML = "Generatore password migliore al mondo";
 document.getElementById("subtitle").innerHTML = "La tua password è :";
 
 let chars = "0bc1qk55vk7wjgzg3pmxlh59rv5dlgewd9jem5nrt4w!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let passwordLength = 12;
-let password = "";
+let passwordLength ;
 
-for(let i=0; i <= passwordLength; i++){
-    let randomNumber = Math.floor(Math.random() * chars.length);
-    password = password + chars.substring(randomNumber , randomNumber + 1);
+function generate(){
+    let result = "";
+    passwordLength = document.getElementById("lenghtPass").value;
+    const charactersLength = chars.length;
+    for(let i=0; i <= passwordLength; i++){
+        result += chars.charAt(Math.floor(Math.random() * charactersLength));
+     }
+
+     document.getElementById("suggpass").innerHTML = result
 }
 
-console.log(password);
 
-document.getElementById("password").innerHTML = password;
